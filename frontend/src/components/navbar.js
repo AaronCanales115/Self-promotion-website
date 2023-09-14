9cc78e8d961461ba94c60d1b812011c05e82adb0
Aaron Canales <aaroncanales0107@gmail.com>
2023-09-13 19:07:56 -0700

    frontend setup, navbar added

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,44 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function navbar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/explore">Explore</Nav.Link>
+            <Nav.Link href="/about">About us</Nav.Link>
+            <NavDropdown title="Account" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="/signup">Sign up</NavDropdown.Item>
+              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+              <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default navbar;
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,11 @@
+
+const Home = () =>{
+    return <div className="home-container">
+        <div>
+            <h1>This is the home page</h1>
+        </div>
+    </div>
+}
+
+
+export default Home
